6b779d2f5195ff61cecf1e6b008136ceba66e417
liushuai <liu.5431@qq.com>
2018-11-22 00:17:36 +0800

    标记master

--- src/main/java/cn/yxbd/demo/DemoController.java
+++ src/main/java/cn/yxbd/demo/DemoController.java
@@ -31,7 +31,7 @@ public class DemoController {
             Object o = properties.get(obj);
             System.out.println(obj.toString() + "\r\n\t" + o.toString());
         }
-
+        System.out.println("dev分支");
     }
 
     public String getName() {
