3eec1d20eaad4ab7aa23a016dc3c30b29b614604
liushuai <liu.5431@qq.com>
2018-11-22 00:09:56 +0800

    æäº¤ä¿¡æ¯

--- src/main/java/cn/yxbd/demo/DemoController.java
+++ src/main/java/cn/yxbd/demo/DemoController.java
@@ -59,3 +59,15 @@ public class DemoController {
         this.age = age;
     }
 }
+
+class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
