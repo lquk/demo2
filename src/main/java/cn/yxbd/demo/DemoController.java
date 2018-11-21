6a8bacf386ce4f2c1a1bf39f9efe6165f717cf71
liushuai <liu.5431@qq.com>
2018-11-21 23:12:15 +0800

    hah

--- src/main/java/cn/yxbd/demo/DemoController.java
+++ src/main/java/cn/yxbd/demo/DemoController.java
@@ -32,6 +32,7 @@ public class DemoController {
             System.out.println(obj.toString() + "\r\n\t" + o.toString());
         }
         System.out.println("push");
+
     }
 
     public String getName() {
